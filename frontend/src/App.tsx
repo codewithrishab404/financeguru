606c97af8d2070decb791b18626355d87a769882
aritra biswas <aritrabiswas0085@gmail.com>
2025-09-20 14:47:15 +0530

    contact pagr

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./Pages/LoginPage"; // create this component
 import Home from "./Pages/Home";
+import Contact from "./Component/Contact";
 // move your current ChatGuru content here
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
